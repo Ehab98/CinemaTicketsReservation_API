e7b1d09c6c232935fe43581dd75e065d63d2a8dc
Ehab98 <hoba.reda98@gmail.com>
2022-01-10 17:08:46 +0200

    model of ticket

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+
+class Movie(models.Model):
+
+    hall = models.CharField(max_length=50)
+    movie = models.CharField(max_length=50)
+    date = models.DateField()
+
+
+    class Meta:
+        verbose_name = ("Movie")
+        verbose_name_plural = ("Movies")
+
+    def __str__(self):
+        return self.movie
+
+
+
+class Guest(models.Model):
+
+    
+    name = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=50)
+    class Meta:
+        verbose_name = ("Guest")
+        verbose_name_plural = ("Guests")
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+
+    
+    guest = models.ForeignKey("Guest",related_name='reservation', on_delete=models.CASCADE)
+    movie = models.ForeignKey("Movie",related_name='reservation', on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = ("Reservation")
+        verbose_name_plural = ("Reservations")
+
+    def __str__(self):
+        return self.movie
+
